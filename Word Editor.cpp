7338fb6b3e5ea7051e8e13670402c30dfdec226c
Occy <44588713+OccySurf94@users.noreply.github.com>
2019-02-03 06:39:11 -0800

    Update Word Editor.cpp
    Added a for loop which shows each character of the word, making it easier to change a specific character

--- Word Editor.cpp
+++ Word Editor.cpp
@@ -1,83 +1,89 @@
 #include <iostream>
 #include <string>
+#include <iostream>
+#include <string>
 #include <Windows.h>
 
 using namespace std;
 
 int main()
 {
-  string word; // Word to change
-  int character; // Which character to change
-  char wordChange; // What to replace the character with
-  cout << "Enter a word smaller than 11 characters: ";
-  cin >> word;
-  if (word[10]) // Run if the word is over 10 characters
-  {
-      cout << "\nINVALID WORD!" << endl;
-      Sleep(4000);
-      return 0;
-  }
-  cout << "\nWord is " << word << endl;
-  cout << "\nChoose a number which represents a character in the word (1 = 1st Character): ";
-  cin >> character;
-  cout << "\nWhat would you like to replace it with?: ";
-  cin >> wordChange;
-  
- switch(character)
- {
-  case 1:
-    word[0] = wordChange;
-    cout << "\nWord is now " << word;
-    break;
-    
-  case 2:
-    word[1] = wordChange;
-    cout << "\nWord is now " << word;
-    break;
-    
-  case 3:
-    word[2] = wordChange;
-    cout << "\nWord is now " << word;
-    break;
-    
-  case 4:
-    word[3] = wordChange;
-    cout << "\nWord is now " << word;
-    break;
+	string word; // Word to change
+	int character; // Which character to change
+	char wordChange; // What to replace the character with
+	cout << "Enter a word smaller than 11 characters: ";
+	cin >> word;
+	if (word[10]) // Run if the word is over 10 characters
+	{
+		cout << "\nINVALID WORD!" << endl;
+		Sleep(3000);
+		return 0;
+	}
+	cout << "\nWord is " << word << endl;
+	for (unsigned int i = 0; i < word.size(); i++) // Show each character 
+	{
+		cout << "\nCharacter at postion " << i << " is " << word[i];
+	}
+	cout << "\n\nChoose a number which represents a character in the word (1 = 1st Character): ";
+	cin >> character;
+	cout << "\nWhat would you like to replace it with?: ";
+	cin >> wordChange;
 
-  case 5:
-    word[4] = wordChange;
-    cout << "\nWord is now " << word;
-    break;
-    
-  case 6:
-    word[5] = wordChange;
-    cout << "\nWord is now " << word;
-    break;
+	switch (character)
+	{
+	case 0:
+		word[0] = wordChange;
+		cout << "\nWord is now " << word << endl;
+		break;
 
-  case 7:
-    word[6] = wordChange;
-    cout << "\nWord is now " << word;
-    break;
-    
-  case 8:
-    word[7] = wordChange;
-    cout << "\nWord is now " << word;
-    break;
-    
-  case 9:
-    word[8] = wordChange;
-    cout << "\nWord is now " << word;
-    break;
-    
-  case 10:
-    word[9] = wordChange;
-    cout << "\nWord is now " << word;
-    break;
+	case 1:
+		word[1] = wordChange;
+		cout << "\nWord is now " << word << endl;
+		break;
 
-  default:
-    cout << "\nInvalid Word!";
-    break;
- }
-  return 0;
+	case 2:
+		word[2] = wordChange;
+		cout << "\nWord is now " << word << endl;
+		break;
+
+	case 3:
+		word[3] = wordChange;
+		cout << "\nWord is now " << word << endl;
+		break;
+
+	case 4:
+		word[4] = wordChange;
+		cout << "\nWord is now " << word << endl;
+		break;
+
+	case 5:
+		word[5] = wordChange;
+		cout << "\nWord is now " << word << endl;
+		break;
+
+	case 6:
+		word[6] = wordChange;
+		cout << "\nWord is now " << word << endl;
+		break;
+
+	case 7:
+		word[7] = wordChange;
+		cout << "\nWord is now " << word << endl;
+		break;
+
+	case 8:
+		word[8] = wordChange;
+		cout << "\nWord is now " << word << endl;
+		break;
+
+	case 9:
+		word[9] = wordChange;
+		cout << "\nWord is now " << word << endl;
+		break;
+
+	default:
+		cout << "\nInvalid Word!" << endl;
+		break;
+	}
+	return 0;
 } // Any help on adding longer word edits (not using a switch statement) would be appreciated
